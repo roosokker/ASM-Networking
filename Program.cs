423e05be52028c872970127f48f96fda62823cc7
roosokker <38257648+roosokker@users.noreply.github.com>
2018-12-15 23:38:10 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+namespace DLLTestSampleCSharp
+{
+    class Program
+    {
+       
+        
+        
+
+        static void Main(string[] args)
+        {
+            try
+            {
+               
+            }
+            catch (Exception e)
+            {
+                e.GetType();
+            }
+        }
+    }
+}
